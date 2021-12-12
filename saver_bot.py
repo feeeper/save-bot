85016b070116cb96be66f85db0a8be121196336b
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2021-12-12 21:12:37 +0300

    `register` command instead of `login`

--- saver_bot.py
+++ saver_bot.py
@@ -2,7 +2,7 @@ import json
 
 import boxsdk
 import requests
-from telegram import Bot as TelegramBot, Update
+from telegram import Bot as TelegramBot, Update, ChatAction
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram.utils.request import Request
 
@@ -16,12 +16,14 @@ class SaverBot(TelegramBot):
         self.updater = Updater(bot=self, workers=4)
         self.dispatcher = self.updater.dispatcher
         self.dispatcher.add_handler(CommandHandler('start', self.start))
-        self.dispatcher.add_handler(CommandHandler('login', self.login))
+        self.dispatcher.add_handler(CommandHandler('register', self.register))
 
     def start(self, update: Update, context: CallbackContext):
         message_text: str = update.message.text
         message_parts: list[str] = message_text.split(' ')
         if len(message_parts) == 2:
+            context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+
             code: str = message_parts[1]
             access_token_url = 'https://api.box.com/oauth2/token'
             headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -46,14 +48,14 @@ class SaverBot(TelegramBot):
         else:
             self.show_welcome_message(update, context)
 
-    def login(self, update: Update, context: CallbackContext):
+    def register(self, update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=f'<a href="https://account.box.com/api/oauth2/authorize?client_id={self.config.client_id}&redirect_uri={self.config.redirect_url}&response_type=code">Connect to Box.com</a>',
                                  parse_mode='HTML')
 
     @staticmethod
     def show_welcome_message(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Save to Cloud bot')
 
     def run(self):
         self.updater.start_polling()
