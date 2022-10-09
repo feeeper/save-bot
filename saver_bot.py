82dc039d2d879d5c61cb814f0eb1abecfcb8bae6
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2022-10-09 20:17:47 +0300

    bump tba to 20.0a4, rewrite bot to Application

--- saver_bot.py
+++ saver_bot.py
@@ -3,12 +3,10 @@ import json
 import boxsdk
 from boxsdk.object.user import User as BoxUser
 from boxsdk.object.folder import Folder as BoxFolder
-from boxsdk.object.file import File as BoxFile
 
 import requests
-from telegram import Bot as TelegramBot, Update, ChatAction
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram.utils.request import Request
+from telegram import Bot as TelegramBot, Update
+from telegram.ext import CommandHandler, CallbackContext, Application
 
 from config import Config
 
@@ -32,18 +30,14 @@ def get_something_went_wrong_from_box_api_msg_text(box_exception: boxsdk.excepti
 class SaverBot(TelegramBot):
     def __init__(self):
         self.config = Config()
-        super().__init__(self.config.token, request=Request(con_pool_size=8))
-        self.updater = Updater(bot=self, workers=4)
-        self.dispatcher = self.updater.dispatcher
-        self.dispatcher.add_handler(CommandHandler('start', self.start))
-        self.dispatcher.add_handler(CommandHandler('register', self.register))
+        self.application = Application.builder().token(self.config.token).build()
+        self.application.add_handler(CommandHandler('start', self.start))
+        self.application.add_handler(CommandHandler('register', self.register))
 
-    def start(self, update: Update, context: CallbackContext):
+    async def start(self, update: Update, context: CallbackContext):
         message_text: str = update.message.text
         message_parts: list[str] = message_text.split(' ')
         if len(message_parts) == 2:
-            context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-
             code: str = message_parts[1]
             access_token_url = 'https://api.box.com/oauth2/token'
             headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -68,37 +62,27 @@ class SaverBot(TelegramBot):
             try:
                 save_bot_directory: BoxFolder = box_client.root_folder().create_subfolder(get_save_bot_directory_name())
                 
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                        text=get_success_registered_msg_text(user.login, save_bot_directory.object_id),
-                                        parse_mode='HTML')
+                await update.message.reply_html(get_success_registered_msg_text(user.login, save_bot_directory.object_id))
             except boxsdk.exception.BoxAPIException as box_exception:
                 if box_exception.status == 409 and box_exception.code == 'item_name_in_use':
                     context_info = box_exception.context_info
                     conflicts = [x for x in context_info['conflicts'] if x['name'] == get_save_bot_directory_name()]
                     if len(conflicts) == 1:
                         save_bot_directory_object_id = conflicts[0]['id']                        
-                        context.bot.send_message(chat_id=update.effective_chat.id,
-                                                text=get_already_registered_msg_text(user.login, save_bot_directory_object_id),
-                                                parse_mode='HTML')
+                        await update.message.reply_html(get_already_registered_msg_text(user.login, save_bot_directory_object_id))
                     else:
-                        context.bot.send_message(chat_id=update.effective_chat.id,
-                                            text=get_something_went_wrong_from_box_api_msg_text(box_exception),
-                                            parse_mode='HTML')
+                        await update.message.reply_html(get_something_went_wrong_from_box_api_msg_text(box_exception))
                 else:
-                    context.bot.send_message(chat_id=update.effective_chat.id,
-                                            text=get_something_went_wrong_from_box_api_msg_text(box_exception),
-                                            parse_mode='HTML')
+                    await update.message.reply_html(get_something_went_wrong_from_box_api_msg_text(box_exception))
         else:
-            self.show_welcome_message(update, context)
+            await self.show_welcome_message(update, context)
 
-    def register(self, update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f'<a href="https://account.box.com/api/oauth2/authorize?client_id={self.config.client_id}&redirect_uri={self.config.redirect_url}&response_type=code">Connect to Box.com</a>',
-                                 parse_mode='HTML')
+    async def register(self, update: Update, context: CallbackContext):
+        await update.message.reply_html(f'<a href="https://account.box.com/api/oauth2/authorize?client_id={self.config.client_id}&redirect_uri={self.config.redirect_url}&response_type=code">Connect to Box.com</a>')
 
     @staticmethod
-    def show_welcome_message(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Save to Cloud bot')
+    async def show_welcome_message(update: Update, context: CallbackContext):
+        await update.message.reply_text('Save to Cloud bot')
 
     def run(self):
-        self.updater.start_polling()
+        self.application.run_polling()
