c5f5bffd5f76d36ff243a61a181be8f806ead58f
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2022-10-23 13:58:10 +0300

    basic upload file + cleaned persistence

--- sqlite_persistence.py
+++ sqlite_persistence.py
@@ -21,8 +21,10 @@ def init_db(name: str = 'save_bot.db') -> None:
 
 
 class SqlitePersistence(BasePersistence):
-    def __init__(self, name: str='save_bot.db'):
-        self.store_data = PersistenceInput(bot_data=False, user_data=False, callback_data=False)
+    def __init__(self, name: str='save_bot.db'): 
+        super().__init__(
+            store_data=PersistenceInput(bot_data=False, user_data=False, callback_data=False),
+            update_interval=1)
         self.conn = sqlite3.connect(name)
         self.cursor = self.conn.cursor()
 
@@ -32,24 +34,13 @@ class SqlitePersistence(BasePersistence):
         return chat_data
 
     async def update_chat_data(self, chat_id: int, data: CD) -> None:
-        should_update = False
-
         if isinstance(data, dict) and len(data) > 0:
             access_token = data['access_token']
             refresh_token = data['refresh_token']
-            box_folder_id = data['box_folder_id']            
-            should_update = True
+            box_folder_id = data.get('box_folder_id', None)            
 
-        if should_update:
-            row = self.cursor.execute('''SELECT EXISTS(SELECT * FROM chat_data WHERE chat_id = ?)''', (chat_id,)).fetchone()
-            if row[0] == 0:                
-                ts = int(datetime.now(tz=zoneinfo.ZoneInfo('UTC')).timestamp())
-                self.cursor.execute('''INSERT INTO chat_data
-                                           (chat_id, active, registration_ts, access_token, refresh_token, box_folder_id)
-                                       VALUES 
-                                           (?, 1, ?, ?, ?, ?)''',
-                    (chat_id, ts, access_token, refresh_token, box_folder_id))
-            else:
+            is_chat_data_exists = self._chat_data_exists(chat_id)
+            if is_chat_data_exists:
                 self.cursor.execute('''UPDATE chat_data
                                        SET
                                            access_token = ?,
@@ -57,18 +48,30 @@ class SqlitePersistence(BasePersistence):
                                            box_folder_id = ?
                                        WHERE
                                            chat_id = ?''', (access_token, refresh_token, box_folder_id, chat_id))
-            self.conn.commit()
+            else:
+                ts = int(datetime.now(tz=zoneinfo.ZoneInfo('UTC')).timestamp())
+                self.cursor.execute('''INSERT INTO chat_data
+                                           (chat_id, active, registration_ts, access_token, refresh_token, box_folder_id)
+                                       VALUES 
+                                           (?, 1, ?, ?, ?, ?)''',
+                    (chat_id, ts, access_token, refresh_token, box_folder_id))
                 
-        return super().update_chat_data(chat_id, data)
+            self.conn.commit()
+
+    def _chat_data_exists(self, chat_id):
+        row = self.cursor.execute('''SELECT EXISTS(SELECT * FROM chat_data WHERE chat_id = ?)''', (chat_id,)).fetchone()
+        return row[0] > 0
 
     async def refresh_chat_data(self, chat_id: int, chat_data: Any) -> None:
         if isinstance(chat_data, dict):
             data = self.cursor.execute('''SELECT * FROM chat_data WHERE chat_id = ?''', (chat_id, )).fetchone()
-            record = dict(zip(['id', 'chat_id', 'active', 'registration_ts', 'access_token', 'refresh_token', 'box_folder_id'], data))
-            chat_data.update(record)
+            if data is not None:
+                record = dict(zip(['id', 'chat_id', 'active', 'registration_ts', 'access_token', 'refresh_token', 'box_folder_id'], data))
+                chat_data.update(record)
 
     async def drop_chat_data(self, chat_id: int) -> None:
-        return await super().drop_chat_data(chat_id)
+        self.cursor.execute('''DELETE FROM chat_data WHERE chat_id = ?''', (chat_id, ))
+        self.conn.commit()
     
     async def get_bot_data(self) -> Any:
         pass
@@ -101,8 +104,7 @@ class SqlitePersistence(BasePersistence):
         pass
 
     def flush(self) -> None:
-        return super().flush()
-    
+        self.conn.close()  
 
     async def drop_user_data(self, user_id: int) -> None:
         pass
