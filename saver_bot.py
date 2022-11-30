e2617b6b016720696d5c877b138778b74dc12e93
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2022-11-30 22:47:29 +0300

    Refactoring: methods were extracted, code was formatted, redundant code was removed (#12)
    * extracted more specific methods
    * ignore .vscode
    * formatting and remove redundant code

--- saver_bot.py
+++ saver_bot.py
@@ -1,14 +1,22 @@
 import io
-import json
 
 import boxsdk
 from boxsdk.object.user import User as BoxUser
 from boxsdk.object.folder import Folder as BoxFolder
+from boxsdk.object.file import File as BoxFile
 
 
 import requests
-from telegram import Bot as TelegramBot, Update
+from telegram import Bot as TelegramBot, Update 
 from telegram.ext import CommandHandler, CallbackContext, Application, BasePersistence, MessageHandler, filters
+from telegram.ext import (
+    CommandHandler,
+    CallbackContext,
+    Application,
+    BasePersistence,
+    MessageHandler,
+    filters
+)
 from telegram.error import BadRequest as tgBadRequest
 
 from config import Config
@@ -40,61 +48,60 @@ class SaverBot(TelegramBot):
 
         self.application = Application.builder().token(self.bot_token).persistence(persistence).build()
         self.application.add_handler(CommandHandler('start', self.start))
-        self.application.add_handler(CommandHandler('register', self.register))
         self.application.add_handler(MessageHandler(filters=filters.ALL, callback=self.upload_file))
 
 
     async def start(self, update: Update, context: CallbackContext):
-        message_text: str = update.message.text
-        message_parts: list[str] = message_text.split(' ')
+        message_parts: list[str] = update.message.text.split(' ')
         if len(message_parts) == 2:
-            code: str = message_parts[1]
-            access_token_url = 'https://api.box.com/oauth2/token'
-            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-            params = {
-                'client_id': self.client_id,
-                'client_secret': self.client_token,
-                'code': code,
-                'grant_type': 'authorization_code'
-            }
-            req = requests.post(access_token_url, data=params, headers=headers)
+            await self.register_user(update, context)
+        else:
+            await self.send_register_link(update, context)
 
-            data = json.loads(req.text)
-            access_token = data['access_token']
-            refresh_token = data['refresh_token']
-            context.chat_data['access_token'] = access_token
-            context.chat_data['refresh_token'] = refresh_token
-
+    async def register_user(self, update, context):        
+        try:
+            access_token, refresh_token = self.get_oauth_tokens(update, context)
             oauth: boxsdk.OAuth2 = boxsdk.OAuth2(self.client_id, self.client_token, access_token=access_token, refresh_token=refresh_token)
             box_client = boxsdk.Client(oauth)
-            user: BoxUser = box_client.user().get()            
+            user: BoxUser = box_client.user().get()
+            save_bot_directory: BoxFolder = box_client.root_folder().create_subfolder(get_save_bot_directory_name())
+            context.chat_data['box_folder_id'] = save_bot_directory.object_id
+            await update.message.reply_html(get_success_registered_msg_text(user.login, save_bot_directory.object_id))
 
-            try:
-                save_bot_directory: BoxFolder = box_client.root_folder().create_subfolder(get_save_bot_directory_name())
-                context.chat_data['box_folder_id'] = save_bot_directory.object_id
-                await update.message.reply_html(get_success_registered_msg_text(user.login, save_bot_directory.object_id))
-
-            except boxsdk.exception.BoxAPIException as box_exception:
-                if box_exception.status == 409 and box_exception.code == 'item_name_in_use':
-                    context_info = box_exception.context_info
-                    conflicts = [x for x in context_info['conflicts'] if x['name'] == get_save_bot_directory_name()]
-                    if len(conflicts) == 1:
-                        save_bot_directory_object_id = conflicts[0]['id']                        
-                        context.chat_data['box_folder_id'] = save_bot_directory_object_id
-                        await update.message.reply_html(get_already_registered_msg_text(user.login, save_bot_directory_object_id))
-                    else:
-                        await update.message.reply_html(get_something_went_wrong_from_box_api_msg_text(box_exception))
+        except boxsdk.exception.BoxAPIException as box_exception:
+            if box_exception.status == 409 and box_exception.code == 'item_name_in_use':
+                context_info = box_exception.context_info
+                conflicts = [x for x in context_info['conflicts'] if x['name'] == get_save_bot_directory_name()]
+                if len(conflicts) == 1:
+                    save_bot_directory_object_id = conflicts[0]['id']
+                    context.chat_data['box_folder_id'] = save_bot_directory_object_id
+                    await update.message.reply_html(get_already_registered_msg_text(user.login, save_bot_directory_object_id))
                 else:
                     await update.message.reply_html(get_something_went_wrong_from_box_api_msg_text(box_exception))
-        else:
-            await self.show_welcome_message(update, context)
+            else:
+                await update.message.reply_html(get_something_went_wrong_from_box_api_msg_text(box_exception))
 
-    async def register(self, update: Update, context: CallbackContext):
+    def get_oauth_tokens(self, update, context):
+        code = update.message.text.split(' ')[1]
+        access_token_url = 'https://api.box.com/oauth2/token'
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        params = {
+            'client_id': self.client_id,
+            'client_secret': self.client_token,
+            'code': code,
+            'grant_type': 'authorization_code'
+        }
+        response = requests.post(access_token_url, data=params, headers=headers)
+
+        json_response = response.json()
+        access_token = json_response['access_token']
+        refresh_token = json_response['refresh_token']
+        context.chat_data['access_token'] = access_token
+        context.chat_data['refresh_token'] = refresh_token
+        return access_token,refresh_token
+
+    async def send_register_link(self, update: Update, context: CallbackContext):
         await update.message.reply_html(f'<a href="https://account.box.com/api/oauth2/authorize?client_id={self.client_id}&redirect_uri={self.redirect_url}&response_type=code">Connect to Box.com</a>')
-
-    @staticmethod
-    async def show_welcome_message(update: Update, context: CallbackContext):
-        await update.message.reply_text('Save to Cloud bot')
 
     @staticmethod
     async def _send_no_document_passed_message(update: Update):
@@ -115,12 +122,8 @@ class SaverBot(TelegramBot):
 
             try:
                 await update.message.reply_text(f'File {update.effective_message.document.file_name} is uploading to your Box.')
-                document = update.effective_message.document
-                file = await self.application.bot.get_file(document.file_id)
-                with io.BytesIO() as content:
-                    await file.download(out=content)
-                    uploaded_file = box_client.folder(folder_id).upload_stream(content, document.file_name)
-                    await update.message.reply_markdown(f'[{uploaded_file.name}](https://app.box.com/file/{uploaded_file.id}) is now in your Box')
+                uploaded_file = await self._upload_file(update, folder_id, box_client)
+                await update.message.reply_markdown(f'[{uploaded_file.name}](https://app.box.com/file/{uploaded_file.id}) is now in your Box')
             except tgBadRequest as tg_exception:
                 if tg_exception.message == 'File is too big':
                     await update.message.reply_text('Sorry, but the file is too big. Bot supports files less than 20 Mb only.')
@@ -133,6 +136,17 @@ class SaverBot(TelegramBot):
                     await update.message.reply_markdown(f'File with the same name already exists: {existing_file_link}')
             except Exception as ex:
                 await update.message.reply_text(f'Internal Error: {ex}')
+        else:
+            await update.message.reply_text('It seems like you have to connect to Box.com.')
+            await self.send_register_link(update, context)
+
+    async def _upload_file(self, update, folder_id, box_client):
+        document = update.effective_message.document
+        file = await self.application.bot.get_file(document.file_id)
+        with io.BytesIO() as content:
+            await file.download(out=content)
+            uploaded_file: BoxFile = box_client.folder(folder_id).upload_stream(content, document.file_name)
+            return uploaded_file
 
 
     def run(self):
