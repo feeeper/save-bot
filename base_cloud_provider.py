c10effbcc8ca8a6e4a2198c9d7f39ab7b3183e56
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2023-01-08 17:37:52 +0300

    basic functionality for Box.com

---
+++ base_cloud_provider.py
@@ -0,0 +1,26 @@
+import typing as t
+
+class BaseCloudProvider:
+    directory_name: str = 'Save Bot Directory'
+    name: str = 'base'
+
+    def __init__(self, **kwargs) -> None:
+        ...
+
+    def get_registration_link(self) -> str:
+        ...
+
+    def get_directory_link(self, directory_id: str) -> str:
+        ...
+
+    def get_file_link(self, file_id: str, file_name: str) -> str:
+        ...
+
+    def get_oauth_tokens(self, code: str) -> str:
+        ...
+
+    def register_user(self, code: str, update_store_callback: t.Callable[[str, str, str], None]) -> str:
+        ...
+
+    def upload_file(self, access_token: str, refresh_token: str, file) -> t.Any:
+        ...
